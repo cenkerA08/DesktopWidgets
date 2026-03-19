d5804c75086467a6aee9a2edcf2fa5799ebd88d3
cenkerA08 <158097944+cenkerA08@users.noreply.github.com>
2026-03-19 23:12:45 +0100

    new icon

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 version.py — Single source of truth for the app version.
 """
 
-VERSION = "1.0.17"
+VERSION = "1.0.18"
 
 # Your GitHub repo — change this to your actual username/repo
 GITHUB_REPO = "cenkerA08/DesktopWidgets"
