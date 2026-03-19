534bd559e3933d8feadad6f1463c750125d58906
cenkerA08 <158097944+cenkerA08@users.noreply.github.com>
2026-03-19 22:35:37 +0100

    release fix

--- version.py
+++ version.py
@@ -2,7 +2,7 @@
 version.py — Single source of truth for the app version.
 """
 
-VERSION = "1.0.13"
+VERSION = "1.0.14"
 
 # Your GitHub repo — change this to your actual username/repo
 GITHUB_REPO = "cenkerA08/DesktopWidgets"
