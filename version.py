5f8d51626ab191b8b88c61d8a77ebdc5c3827a15
cenkerA08 <158097944+cenkerA08@users.noreply.github.com>
2026-03-19 15:52:26 +0100

    Update version.py
    Add updater and version system

--- version.py
+++ version.py
@@ -1,9 +1,8 @@
 """
 version.py — Single source of truth for the app version.
-Bump this before every release.
 """
 
 VERSION = "1.0.0"
 
 # Your GitHub repo — change this to your actual username/repo
-GITHUB_REPO = "YOUR_USERNAME/DesktopWidget"
+GITHUB_REPO = "cenkerA08/DesktopWidget"
